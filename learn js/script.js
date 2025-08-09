462ef182618170f655fdc29fe7299c2c6322a918
Aman Kumar Pal <142696159+aman0p@users.noreply.github.com>
2025-08-09 22:24:09 +0530

    Delete learn js directory

--- learn js/script.js
+++
@@ -1,9 +0,0 @@
-function addTodo(){
-    const inputEl = document.querySelector("input");
-    const value = inputEl.value;
-
-    const newDivEl= document.createElementr("div");
-    newDivEl.innerHTML = value;
-
-    document.querySelector("body").appendChild(newDivEl);
-}
